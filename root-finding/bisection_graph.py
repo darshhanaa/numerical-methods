b1a0ef6c40019ffd825c7164f5d77bef8b67f8fe
Darshana Ghodasaini <darshanaghodasaini106@gmail.com>
2026-06-17 20:30:44 +0545

    Add graph visualization for bisection method

--- root-finding/bisection_graph.py
+++ root-finding/bisection_graph.py
@@ -8,7 +8,7 @@ x = np.linspace(0, 2, 400)
 y = f(x)
 
 plt.figure(figsize=(8,5))
-plt.plot(x, y, label='f(x) = x³ - x - 2')
+plt.plot(x, y, label='f(x) = x^3 - x - 2')
 plt.axhline(0)
 
 plt.xlabel("x")
@@ -17,5 +17,5 @@ plt.title("Bisection Method Function Graph")
 plt.legend()
 plt.grid(True)
 
-plt.savefig("../graphs/bisection_graph.png")
+plt.savefig("graphs/bisection_graph.png")
 plt.show()
