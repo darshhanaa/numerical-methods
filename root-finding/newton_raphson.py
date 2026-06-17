94f7adde5b9b0ebbefed194f9546be4e5fba5d48
Darshana Ghodasaini <darshanaghodasaini106@gmail.com>
2026-06-17 20:40:05 +0545

    Implement Newton-Raphson method for root finding

---
+++ root-finding/newton_raphson.py
@@ -0,0 +1,18 @@
+def f(x):
+    return x**3 - x - 2
+
+def df(x):
+    return 3*x**2 - 1
+
+x = float(input("Enter initial guess: "))
+i = 0
+
+while True:
+    x_new = x - f(x)/df(x)
+
+    if abs(x_new - x) < 0.0001:
+        break
+
+    x = x_new
+    i += 1
+
